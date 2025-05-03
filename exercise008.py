91ffb643e6915cf3a34adb9dc1a0fd7f74fd4eee
excalord <hyukaruu@icloud.com>
2025-05-02 21:27:15 -0500

    Exercise 08

---
+++ exercise008.py
@@ -0,0 +1,12 @@
+# Random number in a context generation
+import random
+
+first_range = int(input("Insert the first number: "))
+second_range = int(input("Insert the limit of the randomize"))
+random_number = random.randint(first_range, second_range)
+
+print(
+    f"You random number is: {random_number}, that is between {first_range} and {
+        second_range
+    }"
+)
