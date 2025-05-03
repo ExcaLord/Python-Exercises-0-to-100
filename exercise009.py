68d39fb30a517ecc9e8c2713aa658b18e3a3407c
excalord <hyukaruu@icloud.com>
2025-05-02 23:51:45 -0500

    Exercise 09
    in this new version we ask the user for the numbers to be compared and after that store them into a list of floats and realize the search of the highest number

--- exercise009.py
+++ exercise009.py
@@ -1,6 +1,17 @@
-# Find the highest number #
+# Highest number from user input #
 
-list_of_numbers = [34, 45, 23, 657, 234, 2345, 2736, 7682]
+list_of_numbers = []
+
+while True:
+    number_input = input(
+        "Insert the numbers you want to find the highest one and done to finish the "
+        "input of numbers: "
+    )
+
+    if number_input == "done" or number_input == "Done":
+        break
+    list_of_numbers.append(float(number_input))
+
 highest_number = list_of_numbers[0]
 
 for numbers in list_of_numbers:
