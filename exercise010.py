0ed00363a83939bc3ca9a73010242b80afd32c17
excalord <hyukaruu@icloud.com>
2025-05-03 02:49:52 -0500

    Exercise 10
    the exercise was solved with the buble sort method

--- exercise010.py
+++ exercise010.py
@@ -4,10 +4,20 @@ list_of_numbers = []
 
 while True:
     user_input = input(
-        "Digit the numbers that will be organize for decimals use point instead of comma: "
+        "Digit the numbers that will be organize for decimals use point: "
     )
     if user_input == "done" or user_input == "Done":
         break
     list_of_numbers.append(float(user_input))
 
-print(sorted(list_of_numbers))
+n = len(list_of_numbers)
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if list_of_numbers[j] > list_of_numbers[j + 1]:
+            list_of_numbers[j], list_of_numbers[j + 1] = (
+                list_of_numbers[j + 1],
+                list_of_numbers[j],
+            )
+
+print(list_of_numbers)
